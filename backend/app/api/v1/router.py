324b3aa08d7d97892514ae50db36edd3864abee8
RodrigoHBK <rodrigohonda@live.com>
2026-03-18 17:52:26 -0300

    feat: registrar as rotas no roteador principal spring 1 - tarefa 1.5

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import health
+from app.api.v1.endpoints import auth, products , health
 
 api_router = APIRouter()
 api_router.include_router(health.router, tags=["health"])
+api_router.include_router(auth.router, tags=["auth"])
+api_router.include_router(products.router, tags=["products"])
